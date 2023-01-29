3dff8505abc19008b4d3ea5eacfacf75f281b656
mirakov <holturaevm@gmail.com>
2023-01-29 22:00:07 +0500

    stopped here because the microphone of my pc is not working, so i can not test audio of alan studio project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "75feabe3c9999bc9d31326d74c6becbb2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
-  return (
-    <div>Alan AI News Application!</div>
-  )
-}
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("This code was executed");
+        }
+      },
+    });
+  }, []);
+  return <div>Alan AI News Application!</div>;
+};
 
-export default App
+export default App;
